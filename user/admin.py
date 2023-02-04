c096a0ae21e9069fb638058555c63b8a79dc7556
Elodie Meunier <elow.m@live.fr>
2023-02-04 20:48:29 +0100

    [FIX] - Adding missing fields in admin view

--- user/admin.py
+++ user/admin.py
@@ -9,7 +9,7 @@ from .models import User
 class UserAdmin(BaseUserAdmin, TranslationAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password') }),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'professional_email', 'birthday')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
         ('Additional info', {'fields': (
